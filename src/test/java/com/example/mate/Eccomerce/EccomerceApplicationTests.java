82ddfebeade7d629ef8e0526f14335c8b6da990a
Bruno Ferreira <fbrunomarcos@gmail.com>
2023-09-25 15:03:48 -0300

    Inicio Proyecto

---
+++ src/test/java/com/example/mate/Eccomerce/EccomerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mate.Eccomerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EccomerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
