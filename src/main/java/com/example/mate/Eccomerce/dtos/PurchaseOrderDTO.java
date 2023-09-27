df7dc5111087c1b5961c699661ec1b3a1b5ba138
sMarianut <smarianut@gmail.com>
2023-09-27 13:25:03 -0300

    PO, dto y repo

---
+++ src/main/java/com/example/mate/Eccomerce/dtos/PurchaseOrderDTO.java
@@ -0,0 +1,21 @@
+package com.example.mate.Eccomerce.dtos;
+
+import com.example.mate.Eccomerce.models.PurchaseOrder;
+
+import java.time.LocalDateTime;
+
+public class PurchaseOrderDTO {
+    private long id;
+    private String ticket;
+    private double amount;
+    private LocalDateTime date;
+
+    public PurchaseOrderDTO() {
+    }
+    public PurchaseOrderDTO(PurchaseOrder PO){
+        this.id = PO.getId();
+        this.ticket = PO.getTicket();
+        this.amount = PO.getAmount();
+        this.date = PO.getDate();
+    }
+}
---
+++ src/main/java/com/example/mate/Eccomerce/repositories/PurchaseOrderRepository.java
@@ -0,0 +1,10 @@
+package com.example.mate.Eccomerce.repositories;
+
+import com.example.mate.Eccomerce.models.PurchaseOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface PurchaseOrderRepository extends JpaRepository<PurchaseOrder, Long> {
+
+}
