9bbfcfd7e5f24d848c0d283ade78e0cc587b05f4
DDarioBenitez <reedq2.alt@gmail.com>
2023-10-09 11:04:59 -0300

    todo funcionando

--- src/main/resources/static/web/assets/scripts/product.js
+++ src/main/resources/static/web/assets/scripts/product.js
@@ -2,30 +2,30 @@ const app = Vue.createApp({
     data() {
         return {
             productDetails: null,
-            selectedImage: '', 
-            productImages: [ 
-                '../images/1.png',
-                '../images/2.jpeg',
-                '../images/2.jpeg', 
+            selectedImage: '',
+            productImages: [
+
             ],
-            products: [], 
-            cart: [],
+            products: [],
+            productId: null,
+            productsPage: []
         };
     },
     created() {
-        const productId = new URLSearchParams(window.location.search).get('id');
-        this.loadProductDetails(productId);
+        this.productId = new URLSearchParams(window.location.search).get('id');
+        this.loadProductDetails(this.productId);
         this.cards()
+        this.getData();
     },
     methods: {
-        loadProductDetails(productId) {
-            this.cart = JSON.parse(localStorage.getItem('cart')) ?? [];
-            axios.get(`/api/products/${productId}`)
+        loadProductDetails() {
+            axios.get(`/api/products/${this.productId}`)
                 .then((response) => {
                     this.productDetails = response.data;
+                    this.productImages = this.productDetails.imageCollection;
                     this.productImages.push(response.data.image);
                     if (this.productImages.length > 0) {
-                        this.selectedImage = this.productImages[3];
+                        this.selectedImage = this.productImages[1];
                     }
                 })
                 .catch((error) => {
@@ -44,15 +44,33 @@ const app = Vue.createApp({
                     console.error('Error al obtener los productos', error);
                 });
         },
-
-        local(product, accion) {
-            if (accion == 'add') {
-                this.cart.push({ ...product, quantity: 1 });
-            }
-            localStorage.setItem('cart', JSON.stringify(this.cart))
-        }
+        getData() {
+            axios.get('/api/products')
+                .then((response) => {
+                    const products = response.data;
+                    this.products = products.sort((a, b) => a.id - b.id);
+                    this.categoryProducts = [...new Set(this.products.map(product => product.category))]
+                    this.getProductHome();
+                })
+                .catch((error) => {
+                    console.error('Error al obtener los productos', error);
+                });
+            this.cart = JSON.parse(localStorage.getItem('cart')) ?? [];
+        },
+        getProductHome() {
+            console.log(this.products);
+            this.productsPage = this.products.filter((product) => {
+                return product.discount > 0
+            }).splice(0, 4)
+            console.log(this.productsPage);
+        },
+        formatPrice(number) {
+            let reset = new Intl.NumberFormat('en-En', { style: 'currency', currency: 'USD' })
+            let balanceFormat = reset.format(number)
+            return balanceFormat
+        },
     },
 });
 
 
-app.mount('#prodetails');
+app.mount('#prodetails12');
